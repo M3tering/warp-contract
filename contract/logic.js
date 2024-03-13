3016e23c5a4f1d95e668a6f94e811e812fe3a88e
sir-ct <davidsirct@gmail.com>
2024-03-13 20:34:39 +0100

    adding code to handle registeration

--- contract/logic.js
+++ contract/logic.js
@@ -7,6 +7,7 @@ const GNOSIS_MAINNET = "https://gnosis-rpc.publicnode.com";
 
 //const transactionHash = "0x26296b719bdf4706da093381cc03da11fc91b94e8b7a8e4893433a712b9d650a";
 const PROTOCOL_CONTRACT_ADDRESS = "0x15Fd3b92Eda42b55C7c521DFff5fdeeC5d76D04a";
+const REGISTERATION_PROTOCOL_CONTRACT_ADDRESS = "0xAF7FF053dF6a38F004DCfB964fAE4Bef6f479E6a"
 const REVENUE_EVENT_TOPIC =
   "0xf0a696af71d2857a7dfd350ee22c0a1ce9f94ad4c083bb6d52e95fd2ed7bbd76";
 
@@ -60,18 +61,63 @@ export async function getTransferEvent(transactionHash, lastBlockHeight, tokenId
   if(blockHeight <= lastBlockHeight) throw new ContractError("Block height not valid")
 
   //2. address == protocol adddress in code
-  if(address !== PROTOCOL_CONTRACT_ADDRESS) throw new ContractError("Adrress is Invalid")
+  if(address !== REGISTERATION_PROTOCOL_CONTRACT_ADDRESS) throw new ContractError("Adrress is Invalid")
 
   //3. tokenId == tokenId in state
   if(tokenIdfromEvm !== tokenIdfromState) throw new ContractError("Token Id does not match")
 
-  let energy = amountPaid * tariff
 
   console.log("energy", energy, "amount_paid", amountPaid, "tariff: ",tariff, "tokenId", tokenIdfromEvm)
 
   return {
     energy: energy,
     newBlockHeight: blockHeight
+  };
+}
+
+export async function getRegisterationEvent(transactionHash, lastBlockHeight, tokenIdfromState) {
+  const provider = new ethers.JsonRpcProvider(CHIADO_TESTNET);
+
+  const receipt = await provider.getTransactionReceipt(transactionHash);
+  if (!receipt) {
+    throw new Error("Transaction not found or not yet mined");
+  }
+
+  const logs = receipt.logs.filter((log) => {
+    return (
+      log.topics[0] === REVENUE_EVENT_TOPIC && // Event signature
+      log.address === PROTOCOL_CONTRACT_ADDRESS // Contract address
+    ); 
+  });
+
+  if (logs.length === 0) {
+    throw new ContractError("No Transfer events found")
+  }
+
+  let paymentlog = logs[0]
+
+  let abi = ["event Register(uint256 indexed tokenId, string indexed publicKey, uint256 timestamp, address from)" ];
+  let iface = new ethers.Interface(abi);
+  let parsedLog = iface.parseLog(paymentlog)
+  let blockHeight = paymentlog["blockNumber"]
+  let address = paymentlog["address"]
+
+  let tokenIdfromEvm = Number(parsedLog.args[0])
+  let publicKey = parsedLog.args[1]
+
+  //checks
+  //1. blockHeight > bolckheight from state
+  if(blockHeight <= lastBlockHeight) throw new ContractError("Block height not valid")
+
+  //2. address == protocol adddress in code
+  if(address !== PROTOCOL_CONTRACT_ADDRESS) throw new ContractError("Adrress is Invalid")
+
+  //3. tokenId == tokenId in state
+  if(tokenIdfromEvm !== tokenIdfromState) throw new ContractError("Token Id does not match")
+
+  return {
+    newBlockHeight: blockHeight,
+    public_key: publicKey
   };
 }
 //=== end of evm interaction function ===//
@@ -104,18 +150,26 @@ export function handle_metering(state, action) {
 export async function handle_topup(state, action) {
   // ToDo: handle payment from EVM
   let transactionHash = action.input.data.transaction_hash
-  let lastBlockHeight = state.last_payment_block
+  let lastBlockHeight = state.last_block
   let token_id = state.token_id
   
   let evmTransactionResult = await getTransferEvent(transactionHash, lastBlockHeight, token_id)
 
-  state.last_payment_block = evmTransactionResult.newBlockHeight
+  state.last_block = evmTransactionResult.newBlockHeight
   state.kwh_balance += evmTransactionResult.energy
 
   return {state}
 }
 
-export function handle_registration(state, action) {
-  // ToDo: handle meter registration on EVM
+export async function handle_registration(state, action) {
+  let transactionHash = action.input.data.transaction_hash
+  let lastBlockHeight = state.last_registeration_block
+  let token_id = state.token_id
+
+  let evmTransactionResult = await getRegisterationEvent(transactionHash, lastBlockHeight, token_id)
+
+  state.last_block = evmTransactionResult.newBlockHeight
+  state.public_key = evmTransactionResult.public_key
+  
   return {state}
 }
