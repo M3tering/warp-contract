9f67cb0f83278620da66a1c2eaf3af5f315f7c70
sir-ct <davidsirct@gmail.com>
2024-03-28 07:07:28 +0100

    fix discrepancies in  interact.js

--- tools/interact.js
+++ tools/interact.js
@@ -12,25 +12,6 @@ const tags = [
   { name: "App-Label", value: "M3tering Protocol" },
 ];
 
-<<<<<<< HEAD
-async function interact_register(meterContractSrc) {
-  const contract = warp.contract(meterContractSrc).connect(wallet);
-  const interactionResult = await contract.dryWrite({
-    txHash:
-      "0x1c24c740852988ebf00b3871f5f7b0c6cc24e620a89a834e9d15173143bd5bd",
-    function: "register",
-  });
-  console.log("result", interactionResult);
-}
-
-async function interact_topup(meterContractSrc) {
-  const contract = warp.contract(meterContractSrc).connect(wallet);
-  const interactionResult = await contract.dryWrite({
-    txHash:
-      "0x26296b719bdf4706da093381cc03da11fc91b94e8b7a8e4893433a712b9d650a",
-    function: "topup",
-  });
-=======
 async function interact_evm(contractId, func, txHash) {
   const contract = warp.contract(contractId).connect(wallet);
   const interactionResult = await contract.dryWrite(
@@ -40,7 +21,6 @@ async function interact_evm(contractId, func, txHash) {
     },
     { tags }
   );
->>>>>>> 2de10ba83349514588a0802ef1882da0b08cae41
   console.log("result", interactionResult);
 }
 
@@ -60,11 +40,6 @@ async function interact_meter(meterContractSrc) {
   console.log("result", interactionResult);
 }
 
-<<<<<<< HEAD
-interact_register("FqYq9EnbMfKCKz6s54xusNQ7RylsmTLB_o8u6xYXPU8");
-//interact_meter("FqYq9EnbMfKCKz6s54xusNQ7RylsmTLB_o8u6xYXPU8");
-// interact_topup("FqYq9EnbMfKCKz6s54xusNQ7RylsmTLB_o8u6xYXPU8");
-=======
 // interact_meter("4A3sCQ-fWlzSc17He_mPd_s3QTbPuzzOmrR_RAkPPv8");
 interact_evm(
   "4A3sCQ-fWlzSc17He_mPd_s3QTbPuzzOmrR_RAkPPv8",
@@ -76,4 +51,3 @@ interact_evm(
 //   "topup",
 //   "0x26296b719bdf4706da093381cc03da11fc91b94e8b7a8e4893433a712b9d650a"
 // );
->>>>>>> 2de10ba83349514588a0802ef1882da0b08cae41
